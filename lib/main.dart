4d47f0359fda878c65aa506bb9b80206e7614871
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-03-14 10:09:47 +0300

    first widget creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intro/homepage.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-// Widget => Görüntü sağlayan yapı.
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Row(
-            children: [
-              Column(
-                children: [Text("Merhaba")],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(MaterialApp(home: Homepage()));
 }
